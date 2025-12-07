61cf2b745b2abce5eca69f82d4f660fae64f39b3
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-12-07 13:19:33 +0500

    Add Simple constructor

---
+++ Simple Constructor/app.js
@@ -0,0 +1,27 @@
+class student {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    // console.log("Hello I am constructor");
+  }
+}
+const std = new student("Muneeb","BSE-24F-604");
+console.log(std.name);
+console.log(std.id);
+console.log("***************");
+const std1 = new student("Mohsin","BSE-24F-631");
+console.log(std1.name);
+console.log(std1.id);
+console.log("***************");
+const std2 = new student("Usman","BSE-24F-642");
+console.log(std2.name);
+console.log(std2.id);
+console.log("***************");
+const std3 = new student("Karim","BSE-24F-628");
+console.log(std3.name);
+console.log(std3.id);
+console.log("***************");
+const std4 = new student("Taha","BSE-24F-623");
+console.log(std4.name);
+console.log(std4.id);
+console.log("***************");
