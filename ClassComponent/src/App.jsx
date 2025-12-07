c4ae3e7fc32db3aa06ba977484dc92a06579c21a
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-12-07 12:03:19 +0500

    Class Component

---
+++ ClassComponent/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import React, { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Ha bhai Class Compoenent</h1>
+        <p>Dekh bhai Seekhay ga to kaam aye ga teray</p>
+      </>
+    );
+  }
+}
+// Pahla tareeqa
+// import React from "react";
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Ha bhai Class Compoenent</h1>
+//       </div>
+//     );
+//   }
+// }
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <h1>Ha Bhai a jao class Component seekhay </h1>
+//       </div>
+//     </>
+//   );
+// }
+
+export default App;
