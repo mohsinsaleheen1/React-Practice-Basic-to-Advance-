82a1157226538c7a5769c6ef6b59bc4a78784b07
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-12-09 15:42:04 +0500

    Pass Propsmethod

--- ClassComponent/src/components/ChildClass.jsx
+++ ClassComponent/src/components/ChildClass.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 
 export default class ChildClass extends Component {
-  componentWillUnmount() {
-    console.log("componentWillUnmount Called");
-  }
   render() {
     return (
       <div>
-        <h1>Hey Mohsin Brother</h1>
+        <button onClick={()=>this.props.pMethod("Mohsin","Saleheen")}>Click Me</button>
       </div>
     );
   }
 }
+
+// import React, { Component } from "react";
+
+// export default class ChildClass extends Component {
+//   componentWillUnmount() {
+//     console.log("componentWillUnmount Called");
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hey Mohsin Brother</h1>
+//       </div>
+//     );
+//   }
+// }
--- ClassComponent/src/components/ParentClass.jsx
+++ ClassComponent/src/components/ParentClass.jsx
@@ -2,26 +2,42 @@ import React, { Component } from "react";
 import ChildClass from "./ChildClass";
 
 export default class ParentClass extends Component {
- 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      active: true,
-    };
-  }
-  changeState = () => {
-    this.setState({
-      active: false,
-    });
+  parentMethod = (fname, lname) => {
+    alert(`Ha bhai ${fname} ${lname}`);
   };
   render() {
     return (
       <div>
-        {/* <ChildClass /> */}
-        {this.state.active ? <ChildClass /> : <h1>Component Deleted</h1>}
-        <button onClick={this.changeState}>Change State</button>
+        <ChildClass pMethod={this.parentMethod} />
       </div>
     );
   }
 }
+
+// import React, { Component } from "react";
+// import ChildClass from "./ChildClass";
+
+// export default class ParentClass extends Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       active: true,
+//     };
+//   }
+//   changeState = () => {
+//     this.setState({
+//       active: false,
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         {/* <ChildClass /> */}
+//         {this.state.active ? <ChildClass /> : <h1>Component Deleted</h1>}
+//         <button onClick={this.changeState}>Change State</button>
+//       </div>
+//     );
+//   }
+// }
