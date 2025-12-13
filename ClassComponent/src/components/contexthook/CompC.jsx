16e55d32cfab25a33d54620e001c0f05e6ff2a43
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-12-13 11:45:35 +0500

    UseContextHook

--- ClassComponent/src/components/contexthook/CompC.jsx
+++ ClassComponent/src/components/contexthook/CompC.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ChannelContext, NameContext } from "./CompMain";
 
 const CompC = () => {
+  const myName = useContext(NameContext);
+  const myLearning = useContext(ChannelContext);
   return (
     <div>
       <h1>I am a Component C</h1>
+      <p>{myName}</p>
+      <p>{myLearning}</p>
     </div>
-  )
-}
+  );
+};
 
-export default CompC
+export default CompC;
--- ClassComponent/src/components/contexthook/CompMain.jsx
+++ ClassComponent/src/components/contexthook/CompMain.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { createContext } from "react";
 import CompA from "./CompA";
-
+export const NameContext = createContext();
+export const ChannelContext = createContext();
 const CompMain = () => {
   return (
     <div>
-      <CompA />
+      <NameContext.Provider value={"Adil"}>
+        <ChannelContext.Provider value={"Use Context Hook In React"}>
+          <CompA />
+        </ChannelContext.Provider>
+      </NameContext.Provider>
     </div>
   );
 };
