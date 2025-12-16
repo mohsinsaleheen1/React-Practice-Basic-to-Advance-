8579cc1e5d7ba9d10e8a221f5c285987680cf9de
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-12-16 23:05:17 +0500

    Useparams Hook

--- ReactRouerProj/src/Component/UserDetail.jsx
+++ ReactRouerProj/src/Component/UserDetail.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const UserDetail = () => {
+  const { id } = useParams();
   return (
     <div>
-      <h1>User Details Page</h1>
+      <h1>User {id} Details Page</h1>
     </div>
   );
 };
