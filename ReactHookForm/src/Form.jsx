e4586699664bd2a54d84322129f43ad495770ec7
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2026-01-30 00:35:12 +0500

    Validation Rule and pract..

--- ReactHookForm/src/Form.jsx
+++ ReactHookForm/src/Form.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, control, formState } = useForm();
+  const { errors } = formState;
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -10,21 +12,56 @@ const Form = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Name</label>
         <br />
-        <input type="text" id="name" {...register("name")} />
+        <input
+          type="text"
+          id="name"
+          {...register("name", {
+            required: "Name must be require",
+          })}
+        />
+        {errors.name && <p className="error">{errors.name?.message}</p>}
         <br />
         <br />
         <label htmlFor="email">Email</label>
         <br />
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "Email must be require",
+            pattern: {
+              value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              message: "Invalid Email Address",
+            },
+          })}
+        />
+        {errors.email && <p className="error">{errors.email?.message}</p>}
         <br />
         <br />
         <label htmlFor="age">Age</label>
         <br />
-        <input type="number" id="age" {...register("age")} />
+        <input
+          type="number"
+          id="age"
+          {...register("age", {
+            valueAsNumber: true,
+            required: "Age must be require",
+            min: {
+              value: 18,
+              message: "Age must be at least 18",
+            },
+            max: {
+              value: 25,
+              message: "Age must be at most 25",
+            },
+          })}
+        />
+        {errors.age && <p className="error">{errors.age?.message}</p>}
         <br />
         <br />
         <button type="submit">Submit</button>
       </form>
+      <DevTool control={control} placement="top-left" />
     </div>
   );
 };
